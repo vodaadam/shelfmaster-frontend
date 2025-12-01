d327315d7f023f0f748eae7c6ff047a92abf1883
vodaadam <vodaadam@fit.cvut.cz>
2025-12-01 13:50:27 +0100

    started working on vue frontend, added index and App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    <header>
+      <h1>Shelf Master</h1>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 20px;
+}
+header h1 {
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 40px;
+  color: #2c3e50;
+}
+</style>
